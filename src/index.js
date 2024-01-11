92c69bb633aa17d0d0992bd3e7a9c17c3304f8f5
asakshat <asakshat453@gmail.com>
2024-01-11 20:53:16 +0100

    Refactoring code.

--- src/index.js
+++ src/index.js
@@ -16,15 +16,11 @@ const sendAttendanceMessage = (cronSchedule, guildId, roleId, embed) => {
 		const guild = client.guilds.cache.get(guildId);
 		const role = guild.roles.cache.get(roleId);
 
-		if (!guild || !role) {
-			console.error('Guild or role not found.');
-			return;
-		}
-
-		// Fetch members with the specified role
+		// Fetch members from the server
 		guild.members.fetch().then((members) => {
 			// Send the message to each member's DM if they have the specified role
 			members.each((member) => {
+				// check if member has specified role
 				if (member.roles.cache.has(role.id)) {
 					member.user.send({ embeds: [embed] });
 				}
@@ -39,14 +35,17 @@ client.once('ready', () => {
 	const guildId = '1070105744474644571';
 	const roleId = '1195055503034818672';
 
+	// Embed message constructor
 	const embed = new EmbedBuilder()
 		.setColor(0x0099ff)
 		.setTitle('Attendance Submission')
 		.setDescription("Don't forget to submit your attendance");
 
-	// Use the function to send messages every minute
+	// Use the function to send message at 08:55 every weekday
 	sendAttendanceMessage('55 8 * * 1-5', guildId, roleId, embed);
+	// Use the function to send message at 13:25 every weekday
 	sendAttendanceMessage('25 13 * * 1-5', guildId, roleId, embed);
+	// Use the function to send message every minute
 	sendAttendanceMessage('* * * * *', guildId, roleId, embed);
 });
 
