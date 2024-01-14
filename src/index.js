2bc7624e1503bff727e5415ed075d57e0c6fd2de
asakshat <asakshat453@gmail.com>
2024-01-14 12:08:17 +0100

    Fixed Time-Zone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
-const cron = require('node-cron');
+const CronJob = require('cron').CronJob;
 
 const client = new Client({
 	intents: [
@@ -11,20 +11,18 @@ const client = new Client({
 });
 
 // Function to send a message to a user
-const sendAttendanceMessage = (cronSchedule, guildId, roleId, embed) => {
-	cron.schedule(cronSchedule, () => {
-		const guild = client.guilds.cache.get(guildId);
-		const role = guild.roles.cache.get(roleId);
+const sendAttendanceMessage = (guildId, roleId, embed) => {
+	const guild = client.guilds.cache.get(guildId);
+	const role = guild.roles.cache.get(roleId);
 
-		// Fetch members from the server
-		guild.members.fetch().then((members) => {
-			// Send the message to each member's DM if they have the specified role
-			members.each((member) => {
-				// check if member has specified role
-				if (member.roles.cache.has(role.id)) {
-					member.user.send({ embeds: [embed] });
-				}
-			});
+	// Fetch members from the server
+	guild.members.fetch().then((members) => {
+		// Send the message to each member's DM if they have the specified role
+		members.each((member) => {
+			// check if member has specified role
+			if (member.roles.cache.has(role.id)) {
+				member.user.send({ embeds: [embed] });
+			}
 		});
 	});
 };
@@ -41,12 +39,41 @@ client.once('ready', () => {
 		.setTitle('Attendance Submission')
 		.setDescription("Don't forget to submit your attendance");
 
-	// Use the function to send message at 08:55 every weekday
-	sendAttendanceMessage('55 8 * * 1-5', guildId, roleId, embed);
-	// Use the function to send message at 13:25 every weekday
-	sendAttendanceMessage('25 13 * * 1-5', guildId, roleId, embed);
-	// Use the function to send message every minute
-	sendAttendanceMessage('* * * * *', guildId, roleId, embed);
+	// Schedule the function to run at 08:55 every weekday CET
+	const job1 = new CronJob(
+		'55 8 * * 1-5',
+		() => {
+			sendAttendanceMessage(guildId, roleId, embed);
+		},
+		null,
+		true,
+		'Europe/Brussels'
+	); // Set the timezone to CET (Europe/Berlin)
+
+	// Schedule the function to run at 13:25 every weekday CET
+	const job2 = new CronJob(
+		'25 13 * * 1-5',
+		() => {
+			sendAttendanceMessage(guildId, roleId, embed);
+		},
+		null,
+		true,
+		'Europe/Brussels'
+	);
+	/* 	const job3 = new CronJob(
+		'7 12 * * *',
+		() => {
+			sendAttendanceMessage(guildId, roleId, embed);
+		},
+		null,
+		true,
+		'Europe/Brussels'
+	); */
+
+	// Start the cron jobs
+	job1.start();
+	job2.start();
+	// job3.start();
 });
 
-client.login(process.env.Token);
+client.login(process.env.TOKEN);
