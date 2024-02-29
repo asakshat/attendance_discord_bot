7c661cfb5c9faf1d65de3b52dbe5bcd9a080a84c
asakshat <asakshat453@gmail.com>
2024-02-29 23:52:19 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -10,16 +10,12 @@ const client = new Client({
 	],
 });
 
-// Function to send a message to a user
 const sendAttendanceMessage = (guildId, roleId, embed) => {
 	const guild = client.guilds.cache.get(guildId);
 	const role = guild.roles.cache.get(roleId);
 
-	// Fetch members from the server
 	guild.members.fetch().then((members) => {
-		// Send the message to each member's DM if they have the specified role
 		members.each((member) => {
-			// check if member has specified role
 			if (member.roles.cache.has(role.id)) {
 				member.user.send({ embeds: [embed] });
 			}
@@ -33,7 +29,6 @@ client.once('ready', () => {
 	const guildId = '1070105744474644571';
 	const roleId = '1195055503034818672';
 
-	// Embed message constructor
 	const embed = new EmbedBuilder()
 		.setColor(0x0099ff)
 		.setTitle('Attendance Submission')
@@ -48,7 +43,7 @@ client.once('ready', () => {
 		null,
 		true,
 		'Europe/Brussels'
-	); // Set the timezone to CET (Europe/Berlin)
+	); 
 
 	// Schedule the function to run at 13:25 every weekday CET
 	const job2 = new CronJob(
@@ -70,7 +65,6 @@ client.once('ready', () => {
 		'Europe/Brussels'
 	); */
 
-	// Start the cron jobs
 	job1.start();
 	job2.start();
 	// job3.start();
